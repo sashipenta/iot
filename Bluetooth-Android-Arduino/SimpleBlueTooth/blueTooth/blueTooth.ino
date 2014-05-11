55d16e4e733c6c54b00767e3b2fde6d692454286
Sashi Penta <sashikumar@gmail.com>
2014-05-11 16:28:34 -0700

    Intial version of communication between Androind and Arduino via Bluetooth HC06.

---
+++ Bluetooth-Android-Arduino/SimpleBlueTooth/blueTooth/blueTooth.ino
@@ -0,0 +1,25 @@
+char incomingByte;  // incoming data
+int  LED = 13;      // LED pin
+ 
+void setup() {
+  Serial.begin(9600); // initialization
+  pinMode(LED, OUTPUT);
+  Serial.println("Press 1 to LED ON or 0 to LED OFF...");
+}
+ 
+void loop() {
+      
+  Serial.println("Press 0/1 to LED ON/OFF!");
+      
+  if (Serial.available() > 0) {  // if the data came    
+    incomingByte = Serial.read(); // read byte
+    if(incomingByte == '0') {
+       digitalWrite(LED, LOW);  // if 1, switch LED Off
+       Serial.println("LED OFF. Press 1 to LED ON!");  // print message
+    }
+    if(incomingByte == '1') {
+       digitalWrite(LED, HIGH); // if 0, switch LED on
+       Serial.println("LED ON. Press 0 to LED OFF!");
+    }
+  }
+}
